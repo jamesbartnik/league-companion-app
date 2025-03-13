624551ba3537964778b0fa3ea428c2cc55bc8aaf
James B. <james.bartnik@gmail.com>
2025-03-13 00:27:06 -0700

    fix: add todo items

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,3 +6,15 @@ export default function Home() {
     </main>
   );
 }
+
+/*
+
+Ideas for more pages / more functionality in the app
+  - Allow the user to enter their Username/Tag to search up some user specific stats (https://developer.riotgames.com/apis#account-v1/GET_getByRiotId)
+  - Store user's puuid with Jotai (or another state manager) and use to fetch data like
+    - Champion mastery (https://developer.riotgames.com/apis#champion-mastery-v4/GET_getAllChampionMasteriesByPUUID)
+    - Progressed challenges (https://developer.riotgames.com/apis#lol-challenges-v1/GET_getPlayerData)
+
+  - League of Legends server status (https://developer.riotgames.com/apis#lol-status-v4/GET_getPlatformData)
+
+ */
